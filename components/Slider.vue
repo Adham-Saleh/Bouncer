95d95414588e785c0d4a26cb1f21b79715c8af5e
Adham-Saleh <adhamsaleh055@gmail.com>
2025-02-11 23:54:38 +0200

    feat: landing page slider

--- components/Slider.vue
+++ components/Slider.vue
@@ -1,45 +1,46 @@
 <template lang="pug">
-    .slider.flex.justify-between.items-center(class="gap-[140px] px-[30px]")
-        aside
-            button
-                Icon(:name="items?.leftBtn")
+    .slider.flex.flex-col(class="lg:flex-row justify-between items-center gap-10 lg:gap-[140px] px-[50px]")
+      aside.hidden(class="lg:block")
+          button(:disabled="currentSlide === 0" @click="handleSlider('decrement')")
+              Icon(:name="leftBtn")
 
-        main.flex.items-center(class="gap-[100px]")
-            span.flex.flex-col.justify-between(v-html="items?.leftContent" class="gap-[50px]") 
+      main.flex.overflow-x-auto.flex-wrap.shrink-0.relative.m-auto.max-w-screen
+          .content.flex.flex-col.flex-nowrap(v-for="item, idx in items" class="lg:flex-row items-center flex-wrap gap-10 lg:gap-[100px] text-center lg:text-left" :class="{'hidden': idx !== currentSlide}")
+            span.flex.flex-col.justify-between(v-html="item?.leftContent" class="gap-6 lg:gap-[50px]") 
             
-            img(:src="`/_nuxt/${items?.imgs[0]}`" class="max-w-[420px]")
+            img(:src="`/_nuxt/${item?.image}`" class="max-w-[250px] lg:max-w-[420px]")
+            
+            span.flex.flex-col.justify-between(v-html="item?.rightContent" class="gap-6 lg:gap-[50px]")
 
-            span.flex.flex-col.justify-between(v-html="items?.rightContent" class="gap-[50px]")
+      aside.hidden(class="lg:block")
+        
+          button(:disabled="currentSlide === (items?.length - 1)" @click="handleSlider('increment')")
+              Icon(:name="rightBtn")
 
-        aside 
-            button
-                Icon(:name="items?.rightBtn")
 </template>
 
 <script setup lang="ts">
-// imgs: {
-//     type: Array,
-//   },
-//   leftContent: {
-//     type: String,
-//   },
-//   rightContent: {
-//     type: String,
-//   },
-//   leftBtn: {
-//     type: String,
-//   },
-//   rightBtn: {
-//     type: String,
-//   },
+const currentSlide = ref(0);
 
-// items will be an array which will hold all properties above
-
-const props = defineProps({
+const { items } = defineProps({
   items: {
-    type: Object,
+    type: Array,
+  },
+  leftBtn: {
+    type: String,
+  },
+  rightBtn: {
+    type: String,
   },
 });
+
+const handleSlider = function (method: string) {
+  if (method === "increment") {
+    currentSlide.value += 1;
+  } else {
+    currentSlide.value -= 1;
+  }
+};
 </script>
 
 <style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,22 @@
 <template lang="pug">
-    .container.flex.items-center.w-full.justify-center.mt-5(class="h-[650px] bg-gradient-to-r from-[#FF4858] from-[#AE5DD0] to-[#8F65FF]")
-        Slider(:items="sliderItems")
+    main(class="overflow-hidden max-w-none")
+      .container.flex.items-center.w-full.justify-center.mt-5(class="h-[650px] max-w-none bg-gradient-to-r from-[#FF4858] from-[#AE5DD0] to-[#8F65FF]")
+          Slider(:items="sliderItems" leftBtn="LeftArr" rightBtn="RightArr")
 </template>
 
 <script setup lang="ts">
-const sliderItems = ref({
-  imgs: ["public/images/iphone.png"],
-  leftContent: `<h2 class="text-[48px] text-white font-medium">iPhoneX</h2><p class="max-w-[320px] text-white font-light">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.</p>`,
-//   rightContent: `<h2 class="text-[48px] text-white font-medium">iPhoneX</h2><p class="max-w-[320px] text-white font-light">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.</p>`,
-  leftBtn: "LeftArr",
-  rightBtn: "RightArr",
-});
+const sliderItems = ref([
+  {
+    image: "public/images/iphone.png",
+    leftContent: `<h2 class="text-[48px] text-white font-medium">iPhoneX 1</h2><p class="max-w-[320px] text-white font-light text-[18px]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.</p>`,
+    //   rightContent: `<h2 class="text-[48px] text-white font-medium">iPhoneX</h2><p class="max-w-[320px] text-white font-light">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.</p>`,
+  },
+  {
+    image: "public/images/phone.png",
+    leftContent: `<h2 class="text-[48px] text-white font-medium">iPhoneX 2</h2><p class="max-w-[320px] text-white font-light text-[18px]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.</p>`,
+    //   rightContent: `<h2 class="text-[48px] text-white font-medium">iPhoneX</h2><p class="max-w-[320px] text-white font-light">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.</p>`,
+  },
+]);
 </script>
 
 <style scoped></style>
